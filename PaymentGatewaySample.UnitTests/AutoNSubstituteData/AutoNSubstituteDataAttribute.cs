7b433df3ff4ca1b9a500a0b67c8eb7dde9937345
Rafael Lembo <rafael.lembo@gmail.com>
2018-08-26 17:07:02 -0300

    Adding test projects.

---
+++ PaymentGatewaySample.UnitTests/AutoNSubstituteData/AutoNSubstituteDataAttribute.cs
@@ -0,0 +1,18 @@
+﻿using AutoFixture;
+using AutoFixture.AutoNSubstitute;
+using AutoFixture.Xunit2;
+
+namespace PaymentGatewaySample.UnitTests.AutoNSubstituteData
+{
+    public class AutoNSubstituteDataAttribute : AutoDataAttribute
+    {
+        public AutoNSubstituteDataAttribute()
+            : base(() => new Fixture()
+                .Customize(new AutoNSubstituteCustomization()))
+        {
+            #pragma warning disable CS0618 // Type or member is obsolete
+            Fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+            #pragma warning restore CS0618 // Type or member is obsolete
+        }
+    }
+}
