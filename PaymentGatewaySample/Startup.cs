ca4d586b5f460cbd762e4698ce837ec4aabda347
Rafael Lembo <rafael.lembo@gmail.com>
2018-10-28 03:36:41 -0300

    Enabling swagger through swashbuckle.

--- PaymentGatewaySample/Startup.cs
+++ PaymentGatewaySample/Startup.cs
@@ -18,6 +18,7 @@ using PaymentGatewaySample.Repositories.Context;
 using PaymentGatewaySample.Repositories.Implementation;
 using PaymentGatewaySample.Services.Factories;
 using PaymentGatewaySample.Services.Implementation;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace PaymentGatewaySample
 {
@@ -32,16 +33,47 @@ namespace PaymentGatewaySample
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddJsonOptions(x => 
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Payment Gateway Sample",
+                        Version = "v1",
+                        Description = "A payment Gateway sample. Built using netcore 2.1 and Azure SQL Server database.",
+                    });
+            });
+
+            services.AddMvc().AddJsonOptions(x =>
             {
                 x.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
                 x.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
                 x.SerializerSettings.Formatting = Formatting.Indented;
             }).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            services.AddDbContext<ApplicationDbContext>(options => 
+            services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseLazyLoadingProxies().UseSqlServer(Configuration.GetConnectionString("Database")));
 
+            RegisterDependencies(services);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Explore");
+            });
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+
+        private void RegisterDependencies(IServiceCollection services)
+        {
             services.AddScoped<ISaleProcessor, SaleProcessor>();
             services.Decorate<ISaleProcessor, AntifraudProcessor>();
             services.AddScoped<ITransactionCreator, TransactionCreator>();
@@ -65,17 +97,6 @@ namespace PaymentGatewaySample
 
             services.AddScoped<IMerchantRepository, MerchantRepository>();
             services.AddScoped<ITransactionRepository, TransactionRepository>();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-                app.UseDeveloperExceptionPage();
-            else
-                app.UseHsts();
-
-            app.UseHttpsRedirection();
-            app.UseMvc();
         }
     }
 }
